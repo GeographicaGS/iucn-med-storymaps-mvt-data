2d90386c6ce48dc5ce8ee5781de9cb3b3f6fdd99
cayetanobv <cayetano.benavent@geographica.gs>
2017-04-19 15:03:01 +0200

    fixed export script

---
+++ export_to_geojson.sh
@@ -0,0 +1,39 @@
+#
+#  Author: Cayetano Benavent, 2017.
+#  cayetano.benavent@geographica.gs
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+
+_CRS=4326
+
+declare -a lyrs_arr=(
+    "all_species_count"
+    "endemic_richness"
+    "threatened_count"
+    "ipas"
+  )
+
+printf "\nExporting %s layers...\n" "${#lyrs_arr[@]}"
+
+for lyr in "${lyrs_arr[@]}"
+  do
+    printf "\n\tStart export: %s" "$lyr";
+    ogr2ogr -f GeoJSON -a_srs EPSG:"$_CRS" -dim XY data/geojson/"$lyr".geojson data/shp/"$lyr".shp
+    printf "\n\tFinish export: %s\n" "$lyr";
+    printf "\t------\n";
+  done
