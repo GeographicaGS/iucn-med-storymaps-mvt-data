4e8c573acac0bf773ed415eaf7c599912aef8090
cayetanobv <cayetano.benavent@geographica.gs>
2017-04-19 15:02:45 +0200

    data moved and new ipas layer

--- data/export_to_geojson.sh
+++
@@ -1,5 +0,0 @@
-
-
-ogr2ogr -f GeoJSON -a_srs EPSG:4326 -dim XY geojson/all_species_count.geojson shp/All_species_count.shp
-ogr2ogr -f GeoJSON -a_srs EPSG:4326 -dim XY geojson/endemic_richness.geojson shp/Endemic_richness_count_hex.shp
-ogr2ogr -f GeoJSON -a_srs EPSG:4326 -dim XY geojson/threatened_count.geojson shp/Theatened_richness_count.shp
